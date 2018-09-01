03a6339b5838804bb8c5c3a8dfcc65877c8fd0f3
haimke111 <haimke111@walla.co.il>
2018-09-01 23:51:55 +0300

    undo

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
-
-#password-less:
-ssh-keygen
-ssh-copy-id -i ~/.ssh/id_rsa.pub server2
-
-#without host key checking:
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no server2
